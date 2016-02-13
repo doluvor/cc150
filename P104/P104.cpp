93f50184d9bb6cf03a0ffdbf9c15b17849787383
doluvor <doluvor@126.com>
2016-02-13 22:37:14 +0800

    Add P104 C++ version

---
+++ P104/P104.cpp
@@ -0,0 +1,33 @@
+tion for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if (root == NULL) {
+            return 0;
+        } else if (root->left == NULL && root->right == NULL) {
+            return 1;
+        } else {
+            
+            int leftTreeDepth = 0;
+            int rightTreeDepth = 0;
+            
+            if (root->left != NULL) {
+                leftTreeDepth = maxDepth(root->left);
+            }
+            
+            if (root->right != NULL) {
+                rightTreeDepth = maxDepth(root->right);
+            }
+            
+            return max(leftTreeDepth, rightTreeDepth) + 1;
+        }
+    }
+};
