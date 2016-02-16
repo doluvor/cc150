3f01a145cb96b09519f93e9df3e41f8ae02c645a
doluvor <doluvor@doluvordeMacBook-Air.local>
2016-02-16 23:29:31 +0800

    Add P292 C++ version

---
+++ P292/P292.cpp
@@ -0,0 +1,11 @@
+ass Solution {
+public:
+    bool canWinNim(int n) {
+        
+        if (n % 4 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
