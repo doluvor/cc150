1b7ccce115f3d455b59151d03549a35f311c08e3
doluvor <doluvor@doluvordeMacBook-Air.local>
2016-02-21 23:41:32 +0800

    Add P144 C++ version

---
+++ P144/P144.cpp
@@ -0,0 +1,31 @@
+tion for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        vector<int> result;
+        
+        addValue(root, result);
+        
+        return result;
+    }
+    
+    void addValue(TreeNode *node, vector<int> &result) {
+        
+        if (node == NULL) {
+            return;
+        }
+        
+        result.push_back(node->val);
+        
+        addValue(node->left, result);
+        addValue(node->right, result);
+    }
+};
